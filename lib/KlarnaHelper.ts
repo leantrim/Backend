eb120c8c8c7d4248e148ee052d0ab6148bda8daf
Leutrim <leutrimbunjaku5@gmail.com>
2023-10-16 19:50:29 +0200

    fix klarna and add keys

--- lib/KlarnaHelper.ts
+++ lib/KlarnaHelper.ts
@@ -1,37 +1,52 @@
-import { CartType, ProductType } from "@mediapartners/shared-types/types/ecommerce";
+import {
+  CartType,
+  ProductType,
+} from "@mediapartners/shared-types/types/ecommerce";
+
+const FRONTEND_URL = process.env.KLARNA_FRONTEND_URL;
+const BACKEND_URL = process.env.KLARNA_BACKEND_URL;
+const KLARNA_URL = process.env.KLARNA_URL;
 
 export const getKlarnaOrder = async (order_id: string) => {
   const auth = getKlarnaAuth();
   try {
-    const response = await fetch(`https://api.playground.klarna.com/checkout/v3/orders/${order_id}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: auth,
-      },
-    });
+    const response = await fetch(
+      `${KLARNA_URL}/checkout/v3/orders/${order_id}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: auth,
+        },
+      }
+    );
 
     if (!response.ok) {
       const errorData = await response.json();
-      throw new Error(`HTTP error! status: ${response.status}, message: ${errorData.error_messages}`);
+      throw new Error(
+        `HTTP error! status: ${response.status}, message: ${errorData.error_messages}`
+      );
     }
 
     const data = await response.json();
     return data;
   } catch (error) {
-    console.error('There was a problem with the fetch operation: ', error);
+    console.error("There was a problem with the fetch operation: ", error);
     return error;
   }
 };
 
-export const sendCreateNewOrderToKlarna = async (cartItems: CartType[], product: ProductType) => {
+export const sendCreateNewOrderToKlarna = async (
+  cartItems: CartType[],
+  product: ProductType
+) => {
   const payLoad = buildKlarnaRequest(cartItems, product);
   const auth = getKlarnaAuth();
   try {
-    const response = await fetch('https://api.playground.klarna.com/checkout/v3/orders', {
-      method: 'POST',
+    const response = await fetch(`${KLARNA_URL}/checkout/v3/orders`, {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
         Authorization: auth,
       },
       body: JSON.stringify(payLoad),
@@ -39,13 +54,15 @@ export const sendCreateNewOrderToKlarna = async (cartItems: CartType[], product:
 
     if (!response.ok) {
       const errorData = await response.json();
-      throw new Error(`HTTP error! status: ${response.status}, message: ${errorData.error_messages}`);
+      throw new Error(
+        `HTTP error! status: ${response.status}, message: ${errorData.error_messages}`
+      );
     }
 
     const data = await response.json();
     return data;
   } catch (error) {
-    console.error('There was a problem with the fetch operation: ', error);
+    console.error("There was a problem with the fetch operation: ", error);
     return error;
   }
 };
@@ -53,13 +70,15 @@ export const sendCreateNewOrderToKlarna = async (cartItems: CartType[], product:
 export const buildKlarnaRequest = (cart: CartType[], product: ProductType) => {
   const orderInfo = cart.map((item) => {
     const totalLineOrderAmount = item.quantity * product.price * 100; // Convert to öre
-    const totalTaxAmount = Math.floor(totalLineOrderAmount * (2500 / (10000 + 2500)));
+    const totalTaxAmount = Math.floor(
+      totalLineOrderAmount * (2500 / (10000 + 2500))
+    );
     return {
       reference: `${item.variant.variantType} - ${item.productName}`,
       name: `${item.productName} - ${item.variant.variantType}`,
       quantity: item.quantity,
       unit_price: product.price * 100,
-      quantity_unit: 'pcs',
+      quantity_unit: "pcs",
       tax_rate: 2500,
       total_amount: totalLineOrderAmount,
       total_discount_amount: 0,
@@ -70,30 +89,31 @@ export const buildKlarnaRequest = (cart: CartType[], product: ProductType) => {
 
   const order_amount = cart.reduce(
     (total, item) => total + item.quantity * product.price * 100, // Convert to öre
-    0,
+    0
   );
 
   const totalTaxAmount = Math.floor(order_amount * (2500 / (10000 + 2500)));
+
   const payLoad = {
-    purchase_country: 'SE',
-    purchase_currency: 'SEK',
-    locale: 'se-SV',
+    purchase_country: "SE",
+    purchase_currency: "SEK",
+    locale: "se-SV",
     order_amount: order_amount,
     order_tax_amount: totalTaxAmount,
     order_lines: [...orderInfo],
     merchant_urls: {
-      terms: 'https://www.example.com/terms.html',
-      checkout: 'https://www.example.com/checkout.html?order_id={checkout.order.id}',
-      confirmation: 'https://localhost:3000/orderconfirmation?order_id={checkout.order.id}',
-      push: 'https://www.example.com/api/push?order_id={checkout.order.id}',
+      terms: `${FRONTEND_URL}/terms.html`,
+      checkout: `${FRONTEND_URL}/checkout.html?order_id={checkout.order.id}`,
+      confirmation: `${FRONTEND_URL}/orderconfirmation?order_id={checkout.order.id}`,
+      push: `${BACKEND_URL}/api/ecommerce/klarnaV3/neworder/confirmation/push?order_id={checkout.order.id}`,
     },
   };
   return payLoad;
 };
 
 export function getKlarnaAuth() {
-  const client = 'PK85748_e3ebe8568456';
-  const secret = 'g7lRUO2IyPJbLiDr';
-  const auth = 'Basic ' + Buffer.from(client + ':' + secret).toString('base64');
+  const client = process.env.KLARNA_CLIENT_KEY; // TODO: Update with production client key
+  const secret = process.env.KLARNA_SECRET_KEY;
+  const auth = "Basic " + Buffer.from(client + ":" + secret).toString("base64");
   return auth;
 }
--- routes/ecommerce/klarnaV3.ts
+++ routes/ecommerce/klarnaV3.ts
@@ -12,16 +12,23 @@ const router = express.Router();
 
 router.get("/:id", auth, async (req: Request, res: Response) => {
   if (!req.params.id) return res.status(400).send("Order ID is missing.");
+  console.log("Request came in..");
 
   try {
     const klarnaData = await getKlarnaOrder(req.params.id);
-    const { merchant_urls, html_snippet, ...restBody } = klarnaData;
-    const order = new Order(restBody);
-    order.save();
     return res.status(200).send(klarnaData);
   } catch (error) {
     return res.status(400).send(error);
   }
+});
+
+// Confirm order
+router.post("/confirmation/push", auth, async (req: Request, res: Response) => {
+  const { merchant_urls, html_snippet, ...restBody } = req.body;
+  console.log(req.body, "req came in");
+  const order = new Order(restBody);
+  order.save();
+  res.status(200);
 });
 
 // Create new order
