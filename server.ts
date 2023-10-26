98c91b0627a744c3698a30a1b26dbe59ff4cc41e
Leutrim <leutrimbunjaku5@gmail.com>
2023-10-26 19:18:16 +0200

    added keys

--- server.ts
+++ server.ts
@@ -1,19 +1,21 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
-import "dotenv/config";
-import user from "./routes/user";
-import auth from "./routes/auth";
-import site from "./routes/LandingPages/site";
-import forms from "./routes/LandingPages/forms";
-import upload from "./routes/Storage/upload";
-import products from "./routes/ecommerce/products";
-import klarnaV3 from "./routes/ecommerce/klarnaV3";
-import store from "./routes/ecommerce/stores/stores";
-import categories from "./routes/ecommerce/stores/categories";
-import review from "./routes/ecommerce/stores/reviews";
-import subPages from "./routes/ecommerce/stores/subPages";
-import { API_ROUTES } from "@mediapartners/shared-types/types/Routes";
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import 'dotenv/config';
+import user from './routes/user';
+import auth from './routes/auth';
+import site from './routes/LandingPages/site';
+import forms from './routes/LandingPages/forms';
+import upload from './routes/Storage/upload';
+import products from './routes/ecommerce/products';
+import klarnaV3 from './routes/ecommerce/klarnaV3';
+import store from './routes/ecommerce/stores/stores';
+import categories from './routes/ecommerce/stores/categories';
+import review from './routes/ecommerce/stores/reviews';
+import subPages from './routes/ecommerce/stores/subPages';
+import { API_ROUTES } from '@mediapartners/shared-types/types/Routes';
+import https from 'https';
+import fs from 'fs';
 
 checkJwtSecret();
 
@@ -44,50 +46,55 @@ connectToMongoDB(MONGODB);
 
 // Function to check if JWT Secret is set
 function checkJwtSecret() {
-  if (!process.env.JWT_SECRET) {
-    console.error("ERROR: JWT Secret not set");
-    process.exit(1);
-  }
+	if (!process.env.JWT_SECRET) {
+		console.error('ERROR: JWT Secret not set');
+		process.exit(1);
+	}
 }
 
 // Function to initialize Express App
 function initializeExpressApp() {
-  const app = express();
-  app.use(cors());
-  app.use(express.json());
-  app.get("/", (req, res) => {
-    res.status(403).send("hello world");
-  });
-  return app;
+	const app = express();
+	app.use(cors());
+	app.use(express.json());
+	app.get('/', (req, res) => {
+		res.status(403).send('hello world');
+	});
+	return app;
 }
 
 // Function to start the server
-function startServer(app: any) {
-  app.listen(8000, () => {
-    console.log("Server is running on port 8000");
-  });
+function startServer(app: express.Application) {
+	const options = {
+		key: fs.readFileSync('./keys/private.key'),
+		cert: fs.readFileSync('./keys/certificate.crt'),
+	};
+
+	https.createServer(options, app).listen(8000, () => {
+		console.log('Server is running on port 8000');
+	});
 }
 
 // Function to get MongoDB configuration
 export function getMongoDBConfig() {
-  return {
-    user: process.env.MONGODB_USER,
-    password: encodeURIComponent(process.env.MONGODB_PASSWORD!!),
-    ip: process.env.MONGODB_IP,
-    port: process.env.MONGODB_PORT,
-    db: process.env.MONGODB_DB,
-  };
+	return {
+		user: process.env.MONGODB_USER,
+		password: encodeURIComponent(process.env.MONGODB_PASSWORD!!),
+		ip: process.env.MONGODB_IP,
+		port: process.env.MONGODB_PORT,
+		db: process.env.MONGODB_DB,
+	};
 }
 
 // Function to connect to MongoDB
 function connectToMongoDB(MONGODB: any) {
-  mongoose.set("strictQuery", false);
-  mongoose
-    .connect(
-      `mongodb://${MONGODB.user}:${MONGODB.password}@${MONGODB.ip}:${MONGODB.port}/${MONGODB.db}?authSource=${MONGODB.db}&authMechanism=DEFAULT`
-    )
-    .then(() => console.log("Connected to MongoDB..."))
-    .catch((err) => {
-      console.log("Could not connect to MongoDB...", err), process.exit(1);
-    });
+	mongoose.set('strictQuery', false);
+	mongoose
+		.connect(
+			`mongodb://${MONGODB.user}:${MONGODB.password}@${MONGODB.ip}:${MONGODB.port}/${MONGODB.db}?authSource=${MONGODB.db}&authMechanism=DEFAULT`
+		)
+		.then(() => console.log('Connected to MongoDB...'))
+		.catch((err) => {
+			console.log('Could not connect to MongoDB...', err), process.exit(1);
+		});
 }
